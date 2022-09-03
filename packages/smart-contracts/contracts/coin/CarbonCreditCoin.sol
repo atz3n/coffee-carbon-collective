8fa3fd6cbfcb914df5fe81e80fe6f16f82e37e29
atz3n <attila.aldemir@atz3n.dev>
2022-09-03 13:22:18 +0200

    rename carbon credit coin

--- packages/smart-contracts/contracts/coin/CarbonCreditCoin.sol
+++ packages/smart-contracts/contracts/coin/CarbonCreditCoin.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CarbonCreditToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Carbon Credit Token", "CCT") {}
+contract CarbonCreditCoin is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Carbon Credit Coin", "CCC") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
