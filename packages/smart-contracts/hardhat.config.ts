e5d1f02e7457cd218b6b73327aaa418fa445ba5b
eroo36 <edogruca@hotmail.com>
2022-09-04 00:33:04 +0300

    feat(hardhat.config): add celo testnet config

--- packages/smart-contracts/hardhat.config.ts
+++ packages/smart-contracts/hardhat.config.ts
@@ -4,17 +4,18 @@ import "hardhat-contract-sizer";
 import "hardhat-gas-reporter";
 import { HardhatUserConfig } from "hardhat/types";
 import "solidity-coverage";
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
     solidity: {
-        version: "0.8.4",
+        version: "0.8.9",
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 200
-            }
-        }
+                runs: 200,
+            },
+        },
     },
     networks: {
         hardhat: {
@@ -22,16 +23,24 @@ const config: HardhatUserConfig = {
         },
         evm: {
             url: process.env.RPC_URL || "",
-            accounts: process.env.DEPLOYER_SECRET !== undefined ? [process.env.DEPLOYER_SECRET] : []
-        }
+            accounts: process.env.DEPLOYER_SECRET !== undefined ? [process.env.DEPLOYER_SECRET] : [],
+        },
+        alfajores: {
+            url: process.env.CELO_RPC_URL,
+            accounts: {
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/52752'/0'/0",
+            },
+            chainId: 44787,
+        },
     },
     typechain: {
         outDir: "typechain",
         target: "ethers-v5",
     },
     contractSizer: {
-        runOnCompile: true
-    }
+        runOnCompile: true,
+    },
 };
 
 export default config;
