ce96447e04b8f3428e06d351febcb7437396018d
Eren Doğruca <edogruca@hotmail.com>
2022-09-07 14:16:12 +0300

    Feature/signin screen (#4)
    * feat: add signin form
    
    * refactor: login screen folder
    
    * chore: removed comment lines

---
+++ packages/mobileapp/lib/views/login_screen/login_screen.dart
@@ -0,0 +1,58 @@
+import 'package:carbon_flutter/features/index.dart';
+import 'package:flutter/material.dart';
+import './login_widgets.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({
+    super.key,
+  });
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding:
+              const EdgeInsets.only(left: 15, right: 15, top: 165, bottom: 15),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              textFormWidget("Username"),
+              SizedBox(height: screenSize.height * 0.02),
+              textFormWidget("Password"),
+              SizedBox(height: screenSize.height * 0.01),
+              const Text(
+                "Forgot your password?",
+              ),
+              SizedBox(height: screenSize.height * 0.39),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: CButton(
+                  label: "Sign In",
+                  onTap: () {
+                    debugPrint("Sign In");
+                    //navigate(HomeScreen())
+                  },
+                  expand: true,
+                  kind: CButtonKind.tertiary,
+                  size: CButtonSize.regular,
+                  enable: true,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      // ),
+    );
+  }
+}
