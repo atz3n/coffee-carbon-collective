2d4a5ec9d5c025d6054939dc9c47bc35184195ba
eroo36 <edogruca@hotmail.com>
2022-09-07 16:53:42 +0300

    refactor: bottomnavbar

--- packages/mobileapp/lib/views/global_widgets/bottom_navigation_bar.dart
+++ packages/mobileapp/lib/views/global_widgets/bottom_navigation_bar.dart
@@ -1,26 +1,33 @@
+import 'package:carbon_flutter/features/index.dart';
 import 'package:flutter/material.dart';
 import 'package:mobileapp/core/core_shelf.dart';
 
 BottomNavigationBar bottomNavigationBar(
     int selectedIndex, void Function(int) onTap) {
   return BottomNavigationBar(
-    items: const <BottomNavigationBarItem>[
+    items: <BottomNavigationBarItem>[
       BottomNavigationBarItem(
-        icon: Icon(Icons.home),
+        icon: const Icon(
+          CIcons.home,
+        ),
         label: 'Home',
+        backgroundColor: HexColor("#8D8D8D"),
       ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.business),
+      const BottomNavigationBarItem(
+        icon: Icon(
+          CIcons.wallet,
+        ),
         label: 'Business',
       ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.school),
+      const BottomNavigationBarItem(
+        icon: Icon(CIcons.user),
         label: 'School',
       ),
     ],
     currentIndex: selectedIndex,
     selectedItemColor: HexColor("#161616"),
+    backgroundColor: HexColor("#8D8D8D"),
     onTap: onTap,
-    unselectedItemColor: HexColor("#8D8D8D"),
+    unselectedItemColor: Colors.white,
   );
 }
