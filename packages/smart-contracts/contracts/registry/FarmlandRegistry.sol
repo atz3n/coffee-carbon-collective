cfd5f0af68e57928e49aab4ea09e9c43f7c823db
atz3n <attila.aldemir@atz3n.dev>
2022-09-10 11:20:30 +0200

    feat(farmlandRegistry): add token URI functionality

--- packages/smart-contracts/contracts/registry/FarmlandRegistry.sol
+++ packages/smart-contracts/contracts/registry/FarmlandRegistry.sol
@@ -3,18 +3,17 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract FarmlandRegistry is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+contract FarmlandRegistry is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     constructor() ERC721("FarmlandRegistry", "FARM") {}
 
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
     }
-
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
         require(msg.sender == owner(), "FarmlandRegistry: Only transferable by contract owner");
@@ -53,5 +52,15 @@ contract FarmlandRegistry is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override(ERC721) {
         _safeTransfer(from, to, tokenId, data);
+    }
+
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
+        _setTokenURI(tokenId, _tokenURI);
     }
 }
---
+++ packages/smart-contracts/test/farmlandRegistry.tokenURI.test.ts
@@ -0,0 +1,42 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { FarmlandRegistry } from "../typechain";
+import { deploy } from "../utils/deployer";
+import { untilSettled } from "../utils/txHelper";
+
+
+describe("FarmlandRegistry tokenURI", function() {
+    let alice: SignerWithAddress;
+    let bob: SignerWithAddress;
+
+    this.beforeAll(async () => {
+        const signers = await ethers.getSigners();
+        alice = signers[0];
+        bob = signers[1];
+    });
+
+
+    it("Should set a token URI", async () => {
+        const dummyTokenURI = "hello world";
+        const farmlandRegistry = await deploy<FarmlandRegistry>("FarmlandRegistry");
+        await untilSettled(farmlandRegistry.safeMint(bob.address, 1));
+        
+        await untilSettled(farmlandRegistry.setTokenURI(1, dummyTokenURI));
+        const tokenURI = await farmlandRegistry.tokenURI(1);
+
+        expect(tokenURI).to.equal(dummyTokenURI);
+    });
+
+    
+    it("Should throw while trying to set a token URI with none contract owner account", async () => {
+        const dummyTokenURI = "hello world";
+        let farmlandRegistry = await deploy<FarmlandRegistry>("FarmlandRegistry");
+        await untilSettled(farmlandRegistry.safeMint(bob.address, 1));
+        farmlandRegistry = farmlandRegistry.connect(bob);
+
+        await expect(
+            farmlandRegistry.setTokenURI(1, dummyTokenURI)
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+});
