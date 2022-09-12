36620a516601123667e944dd2d8c4cd521e660a5
atz3n <attila.aldemir@atz3n.dev>
2022-09-12 11:13:14 +0200

    chore(local-demo): add volume pruning

--- local-demo/scripts/stop-demo.sh
+++ local-demo/scripts/stop-demo.sh
@@ -27,5 +27,6 @@ echo "[INFO] Stopping and removing docker containers..."
 cd ${HERE}/../config
 ${SUDO} docker-compose -p ccc-backend -f docker-compose-local-backend.yml down
 ${SUDO} docker-compose -p ccc-chain -f docker-compose-local-chain.yml down
+${SUDO} docker volume prune -f
 
 echo "[INFO] Done."
