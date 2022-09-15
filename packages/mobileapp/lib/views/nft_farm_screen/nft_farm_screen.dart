8e013566696ea1981f48baf6a9a7d721080707f1
eroo36 <edogruca@hotmail.com>
2022-09-15 23:15:34 +0300

    feat: add nftImageCard

--- packages/mobileapp/lib/views/nft_farm_screen/nft_farm_screen.dart
+++ packages/mobileapp/lib/views/nft_farm_screen/nft_farm_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carbon_flutter/features/index.dart';
 import 'package:flutter/material.dart';
 
 import '../../core/core_shelf.dart';
@@ -30,10 +31,52 @@ class _NftFarmScreenState extends State<NftFarmScreen> {
               title: STitle(label: "NFTLand", center: true),
               showSearchBar: false,
               leadingBack: true,
-              username: "JohnFarmer")
+              username: "JohnFarmer"),
+          SliverToBoxAdapter(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [nftImageCard()]),
+          )
         ]),
         bottomNavigationBar: bottomNavigationBar(_selectedIndex, _onItemTapped),
       ),
     );
   }
+}
+
+Widget nftImageCard() {
+  return Stack(
+    alignment: AlignmentDirectional.bottomCenter,
+    children: [
+      Container(
+        height: 200,
+        color: Colors.grey,
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CButton(
+              kind: CButtonKind.tertiary,
+              label: "NFT Land",
+              onTap: () {},
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.only(
+                    left: 14, top: 12, bottom: 12, right: 95),
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: const Text("Photos"),
+              ),
+            )
+          ],
+        ),
+      )
+    ],
+  );
 }
