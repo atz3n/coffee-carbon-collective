62a91b4db0c2fca6aebd4d7dcb3ff6a839c28219
eroo36 <edogruca@hotmail.com>
2022-09-16 22:02:28 +0300

    feat(nft_farm_screen): complete

--- packages/mobileapp/lib/views/nft_farm_screen/nft_farm_screen.dart
+++ packages/mobileapp/lib/views/nft_farm_screen/nft_farm_screen.dart
@@ -1,8 +1,7 @@
-import 'package:carbon_flutter/features/index.dart';
 import 'package:flutter/material.dart';
+import 'package:mobileapp/views/views_shelf.dart';
 
 import '../../core/core_shelf.dart';
-import '../global_widgets/global_widgets_shelf.dart';
 
 class NftFarmScreen extends StatefulWidget {
   const NftFarmScreen({super.key});
@@ -22,7 +21,6 @@ class _NftFarmScreenState extends State<NftFarmScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // final mediaQuerySize = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         body: CustomScrollView(slivers: [
@@ -33,9 +31,20 @@ class _NftFarmScreenState extends State<NftFarmScreen> {
               leadingBack: true,
               username: "JohnFarmer"),
           SliverToBoxAdapter(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [nftImageCard()]),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              nftImageCard(),
+              tokensTable(total: 52, sold: 10, available: 20, token: "CCT"),
+              farmTags(context, [
+                "Colombia",
+                "Altitude: 1000",
+                "100 ha",
+                "Coffee Quality SCA: 5",
+                "Shaded"
+              ]),
+              farmExpansionPanels(),
+              buyToken()
+            ]),
           )
         ]),
         bottomNavigationBar: bottomNavigationBar(_selectedIndex, _onItemTapped),
@@ -49,18 +58,25 @@ Widget nftImageCard() {
     alignment: AlignmentDirectional.bottomCenter,
     children: [
       Container(
-        height: 200,
-        color: Colors.grey,
-      ),
+          height: 200, decoration: BoxDecoration(color: HexColor("#EBEBEB"))),
       Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CButton(
-              kind: CButtonKind.tertiary,
-              label: "NFT Land",
+            InkWell(
               onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.only(
+                    left: 14, top: 12, bottom: 12, right: 80),
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Colors.black),
+                ),
+                child: const Text("NFT Land",
+                    style: TextStyle(color: Colors.white)),
+              ),
             ),
             InkWell(
               onTap: () {},
@@ -78,5 +94,133 @@ Widget nftImageCard() {
         ),
       )
     ],
+  );
+}
+
+Widget farmTags(BuildContext ctx, List tags) {
+  final mediaQuerySize = MediaQuery.of(ctx).size;
+  return SizedBox(
+    height: mediaQuerySize.height * 0.1,
+    //TODO: change this to grid view
+    child: ListView.builder(
+        itemCount: tags.length,
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        itemBuilder: (BuildContext ctx, i) {
+          return Row(
+            children: [
+              Stack(
+                children: [
+                  Positioned.fill(
+                      child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  )),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        tags[i],
+                        maxLines: 1,
+                        style: const TextStyle(color: Colors.white),
+                      )),
+                ],
+              ),
+              const SizedBox(
+                width: 10,
+              )
+            ],
+          );
+        }),
+  );
+}
+
+Widget farmExpansionPanels() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20),
+    child: ExpansionPanelList(
+      elevation: 1,
+      expandedHeaderPadding: EdgeInsets.zero,
+      children: [
+        ExpansionPanel(
+          canTapOnHeader: true,
+          headerBuilder: (context, isExpanded) => Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              children: const [Text("Regeneration Plan")],
+            ),
+          ),
+          body: const SizedBox(),
+        ),
+        ExpansionPanel(
+            isExpanded: true,
+            canTapOnHeader: true,
+            headerBuilder: (context, isExpanded) => Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Fundraising Start"),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(color: HexColor("#393939")),
+                        child: const Text(
+                          "15.10.2025",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+            body: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+              child: LinearProgressIndicator(
+                value: 0.4,
+                semanticsLabel: 'Linear progress indicator',
+                minHeight: 20,
+                backgroundColor: Colors.grey,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+              ),
+            ))
+      ],
+    ),
+  );
+}
+
+Widget buyToken() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(
+          onTap: () {},
+          child: Container(
+            padding:
+                const EdgeInsets.only(left: 14, top: 12, bottom: 12, right: 55),
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              border: Border.all(color: Colors.black),
+            ),
+            child:
+                const Text("Buy Token", style: TextStyle(color: Colors.white)),
+          ),
+        ),
+        InkWell(
+          onTap: () {},
+          child: Container(
+            padding:
+                const EdgeInsets.only(left: 14, top: 12, bottom: 12, right: 55),
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+            ),
+            child: const Text("Buy Coffee"),
+          ),
+        )
+      ],
+    ),
   );
 }
