3636ef581f2cfbcbc4888d5303323e7b1e990a39
atz3n <attila.aldemir@atz3n.dev>
2022-09-17 20:30:30 +0200

    feat(backend): improve post farmer route

--- packages/backend/src/http/routes/farmers/post/PostFarmersService.ts
+++ packages/backend/src/http/routes/farmers/post/PostFarmersService.ts
@@ -14,12 +14,16 @@ interface Inputs {
     email: string;
 }
 
+interface Outputs {
+    uid: string;
+}
+
 
 export class PostFarmersService implements RouteService {
     constructor(private readonly options: Options) {}
 
 
-    public async run(inputs: Inputs): Promise<void> {
+    public async run(inputs: Inputs): Promise<Outputs> {
         const { address, name, email } = inputs;
 
         const farmer = (await this.options.farmerStore.find({ email }))[0];
@@ -27,12 +31,15 @@ export class PostFarmersService implements RouteService {
             throw new BadRequestError("email already used");
         }
 
+        const uid = randomUUID();
         await this.options.farmerStore.upsert({
             address,
             email,
             farmlands: [],
             name,
-            uid: randomUUID()
+            uid
         });
+
+        return { uid };
     }
 }
